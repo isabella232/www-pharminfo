35680e5a256f979db1c9e8a9658358c41488faf6
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2016-07-26 20:46:15 +0200

    First version of the index page

--- pharminfo.py
+++ pharminfo.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
-from flask import Flask, abort, current_app, render_template, request
+from flask import (
+    Flask, abort, current_app, flash, redirect, render_template, request)
 from jinja2.exceptions import TemplateNotFound
 import mandrill
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'default secret key'
 app.config.from_envvar('WWW_PHARMINFO_CONFIG', silent=True)
-
-ACCESS_TOKEN = app.config.get('ACCESS_TOKEN')
-MANDRILL_KEY = app.config.get('MANDRILL_KEY')
 
 
 @app.route('/')
@@ -39,25 +38,31 @@ def register():
     return render_template('register.html')
 
 
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('404.html'), 404
-
-
-@app.route('/make_contact', methods=['POST'])
-def make_contact():
-    mandrill_client = mandrill.Mandrill(MANDRILL_KEY)
-    form = request.form
+@app.route('/contact', methods=('POST',))
+def contact():
+    if 'phone' in request.form:
+        html = 'Rappeler le numéro {}'.format(request.form['phone'])
+    else:
+        html = '<br>'.join([
+            'Email : %s' % request.form['email'],
+            'Message : %s ' % request.form['message']])
     message = {
         'to': [{'email': 'contact@pharminfo.fr'}],
         'subject': 'Prise de contact sur le site de Pharminfo.fr',
         'from_email': 'contact@pharminfo.fr',
-        'html': '<br>'.join([
-            'Email : %s' % form['email'], 'Nom / Société: %s' % form['name'],
-            'Demande : %s ' % form['question']])}
+        'html': html}
     if not current_app.debug:
+        mandrill_client = mandrill.Mandrill(app.config.get('MANDRILL_KEY'))
         mandrill_client.messages.send(message=message)
-    return ''
+    flash(
+        'Merci de nous avoir contacté, nos équipes vous recontacteront '
+        'dans les plus brefs délais.')
+    return redirect('')
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
