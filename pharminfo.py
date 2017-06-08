e3f00be1f6733da2140bc46ddb6f7eb1861c2981
grewn0uille <lucieanglade.gm@gmail.com>
2017-06-08 10:10:51 +0200

    again

--- pharminfo.py
+++ pharminfo.py
@@ -10,6 +10,7 @@ from xml.etree import ElementTree
 from flask import (Flask, abort, current_app, flash, jsonify, make_response,
                    redirect, render_template, request, url_for)
 from jinja2.exceptions import TemplateNotFound
+
 from mandrill import Mandrill
 from sqlalchemy.orm import joinedload, undefer
 from top_model import db
